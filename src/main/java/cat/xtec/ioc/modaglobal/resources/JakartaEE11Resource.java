48ea73ccc1a595ac73245b858062e7e62dba0fa7
xavier <xavier@EAC4>
2025-12-12 17:04:39 +0100

    Versió 1.0 del projecte web ModaGlobal

---
+++ src/main/java/cat/xtec/ioc/modaglobal/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package cat.xtec.ioc.modaglobal.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
